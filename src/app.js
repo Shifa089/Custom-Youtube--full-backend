bc827c3febc12367a618e8f1a71fcc07c5c84dc6
Shifa Jamadar <shifajamadar14@gmail.com>
2024-02-05 16:57:23 +0530

    controllers setup done

--- src/app.js
+++ src/app.js
@@ -26,4 +26,14 @@ app.use(express.static("public"))
 
 app.use(cookieParser())
 
+//routes import
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+// app.get("/users", (req,res)=>{
+//   res.send("Hello");
+// })
+// http://localhost:8000/api/v1/users/register
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 //require('dotenv').config({path: './env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
@@ -12,7 +13,7 @@ connectDB()
         console.log("ERROR: ",error);
     })
     app.listen(process.env.PORT || 8000,()=>{
-        console.log(`APP listening at POSR ${process.env.PORT}`)
+        console.log(`Server listening at PORT ${process.env.PORT}`)
     })
 })
 .catch((error)=>{
